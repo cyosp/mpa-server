cceacd05ed057af0254746622133bf2203dd62c0
CYOSP <cyosp@users.noreply.github.com>
2017-06-08 23:00:09 +0200

    Add project base files

---
+++ src/test/java/com/cyosp/mpa/MpaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cyosp.mpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MpaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
