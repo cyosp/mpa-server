3fb703cd04a05cfaafd5ef845b022cc7216e8261
CYOSP <cyosp@users.noreply.github.com>
2017-07-25 13:58:00 +0200

    Complete account tag loading

--- src/main/java/com/cyosp/mpa/api/rest/homebank/v1dot2/model/Account.java
+++ src/main/java/com/cyosp/mpa/api/rest/homebank/v1dot2/model/Account.java
@@ -5,19 +5,52 @@ import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 /**
  * Created by CYOSP on 2017-07-23.
  */
 @Getter
 @Setter
-@XStreamAlias(value = "account")
 public class Account {
+
+    @XStreamAsAttribute
+    @XStreamAlias("key")
+    private int key;
+
+    @XStreamAsAttribute
+    @XStreamAlias("flags")
+    private int flags;
+
+    @XStreamAsAttribute
+    @XStreamAlias("pos")
+    private int position;
+
+    @XStreamAsAttribute
+    @XStreamAlias("type")
+    private int type;
+
+    @XStreamAsAttribute
+    @XStreamAlias("curr")
+    private int currencyRef;
 
     @XStreamAsAttribute
     @XStreamAlias("name")
     private String name;
 
     @XStreamAsAttribute
-    @XStreamAlias("key")
-    private int key;
+    @XStreamAlias("initial")
+    private BigDecimal initialBalance;
+
+    @XStreamAsAttribute
+    @XStreamAlias("minimum")
+    private BigDecimal minimumBalance;
+
+    @XStreamAsAttribute
+    @XStreamAlias("cheque1")
+    private long cheque1;
+
+    @XStreamAsAttribute
+    @XStreamAlias("cheque2")
+    private long cheque2;
 }
