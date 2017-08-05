7e4fccd572852981326fb96a3f4c775daf3f1286
CYOSP <cyosp@users.noreply.github.com>
2017-08-05 21:45:35 +0200

    Remove useless interfaces

--- src/main/java/com/cyosp/mpa/api/rest/core/v1/service/MpaService.java
+++ src/main/java/com/cyosp/mpa/api/rest/core/v1/service/MpaService.java
@@ -4,23 +4,115 @@ import com.cyosp.mpa.api.rest.common.exception.DuplicatedNameException;
 import com.cyosp.mpa.api.rest.common.exception.LineNotDeletedException;
 import com.cyosp.mpa.api.rest.common.exception.LineNotInsertedException;
 import com.cyosp.mpa.api.rest.common.exception.LineNotUpdatedException;
+import com.cyosp.mpa.api.rest.core.v1.mapper.AccountMapper;
+import com.cyosp.mpa.api.rest.core.v1.model.Account;
 import com.cyosp.mpa.api.rest.core.v1.request.AccountRequest;
 import com.cyosp.mpa.api.rest.core.v1.response.AccountResponse;
+import lombok.Getter;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by CYOSP on 2017-06-27.
  */
-public interface MpaService {
+@Service
+@Getter
+public class MpaService {
 
-    public AccountResponse addAccount(AccountRequest accountRequest) throws LineNotInsertedException, DuplicatedNameException;
+    @Autowired
+    private AccountMapper accountMapper;
 
-    public List<AccountResponse> getAccounts();
+    //Transactional
+    public Account addAcount(Account account) throws LineNotInsertedException, DuplicatedNameException {
+        try {
+            if (getAccountMapper().addAccount(account) != 1)
+                throw new LineNotInsertedException();
+            account = getAccountMapper().getAccountById(account.getId());
+        } catch (DuplicateKeyException e) {
+            throw new DuplicatedNameException();
+        }
 
-    public AccountResponse getAccountById(long id);
+        return account;
+    }
 
-    public AccountResponse updateAccount(long id, AccountRequest accountRequest) throws LineNotUpdatedException, DuplicatedNameException;
+    public AccountResponse addAccount(AccountRequest accountRequest) throws DuplicatedNameException, LineNotInsertedException {
+        Account account = new Account();
+        account.setName(accountRequest.getName());
+        account.setBalance(new BigDecimal(0));
 
-    public AccountResponse deleteAccount(long id) throws LineNotDeletedException;
+        account = addAcount(account);
+
+        AccountResponse accountResponse = new AccountResponse();
+        BeanUtils.copyProperties(account, accountResponse);
+
+        return accountResponse;
+    }
+
+    public List<AccountResponse> getAccounts() {
+
+        List<AccountResponse> ret = new ArrayList<>();
+
+        for (Account account : getAccountMapper().getAccounts()) {
+            AccountResponse accountResponse = new AccountResponse();
+            BeanUtils.copyProperties(account, accountResponse);
+            ret.add(accountResponse);
+        }
+
+        return ret;
+    }
+
+    public AccountResponse getAccountById(long id) {
+        AccountResponse ret = new AccountResponse();
+
+        Account account = getAccountMapper().getAccountById(id);
+        if (account != null)
+            BeanUtils.copyProperties(account, ret);
+        else
+            ret.setId(AccountResponse.ID_NOT_FOUND);
+
+        return ret;
+    }
+
+    //@Transactional
+    public Account updateAcount(Account account) throws LineNotUpdatedException, DuplicatedNameException {
+        try {
+            if (getAccountMapper().updateAccount(account) != 1)
+                throw new LineNotUpdatedException();
+            account = getAccountMapper().getAccountById(account.getId());
+        } catch (DuplicateKeyException e) {
+            throw new DuplicatedNameException();
+        }
+
+        return account;
+    }
+
+    public AccountResponse updateAccount(long id, AccountRequest accountRequest) throws LineNotUpdatedException, DuplicatedNameException {
+        Account account = new Account();
+        account.setId(id);
+        account.setName(accountRequest.getName());
+
+        account = updateAcount(account);
+
+        AccountResponse accountResponse = new AccountResponse();
+        BeanUtils.copyProperties(account, accountResponse);
+
+        return accountResponse;
+    }
+
+    public AccountResponse deleteAccount(long id) throws LineNotDeletedException {
+
+        if (getAccountMapper().deleteAccount(id) != 1)
+            throw new LineNotDeletedException();
+
+        AccountResponse accountResponse = new AccountResponse();
+        accountResponse.setId(id);
+
+        return accountResponse;
+    }
 }
--- src/main/java/com/cyosp/mpa/api/rest/homebank/v1dot2/service/HomebankService.java
+++ src/main/java/com/cyosp/mpa/api/rest/homebank/v1dot2/service/HomebankService.java
@@ -1,68 +1,214 @@
 package com.cyosp.mpa.api.rest.homebank.v1dot2.service;
 
-import com.cyosp.mpa.api.rest.common.exception.*;
+import com.cyosp.mpa.api.rest.common.exception.DataNotSavedException;
+import com.cyosp.mpa.api.rest.common.exception.DuplicatedNameException;
+import com.cyosp.mpa.api.rest.common.exception.LineNotDeletedException;
+import com.cyosp.mpa.api.rest.common.exception.LineNotUpdatedException;
+import com.cyosp.mpa.api.rest.homebank.v1dot2.mapper.XmlMapper;
+import com.cyosp.mpa.api.rest.homebank.v1dot2.model.*;
 import com.cyosp.mpa.api.rest.homebank.v1dot2.request.AccountRequest;
 import com.cyosp.mpa.api.rest.homebank.v1dot2.response.*;
+import lombok.Getter;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by CYOSP on 2017-06-27.
  */
-public interface HomebankService {
+@Service
+@Getter
+public class HomebankService {
 
-    public void reload();
+    @Autowired
+    private XmlMapper xmlMapper;
 
-    public InfosResponse getInfos();
+    public void reload() {
+        getXmlMapper().loadXmlFile();
+    }
 
-    public PropertiesResponse getProperties();
+    public InfosResponse getInfos() {
+
+        InfosResponse ret = new InfosResponse();
+
+        HomeBank homeBank = getXmlMapper().getInfos();
+        ret.setV(homeBank.getV());
+        ret.setD(homeBank.getD());
+
+        return ret;
+    }
+
+    public PropertiesResponse getProperties() {
+        PropertiesResponse ret = new PropertiesResponse();
+
+        Properties properties = getXmlMapper().getProperties();
+        BeanUtils.copyProperties(properties, ret);
+
+        return ret;
+    }
 
     //
     // Accounts
     //
 
-    public AccountResponse addAccount(AccountRequest accountRequest) throws DataNotSavedException, DuplicatedNameException;
+    public AccountResponse addAccount(AccountRequest accountRequest) throws DataNotSavedException, DuplicatedNameException {
+        Account account = new Account();
+        account.setName(accountRequest.getName());
 
-    public List<AccountResponse> getAccounts();
+        getXmlMapper().addAccount(account);
 
-    public AccountResponse getAccountById(long id);
+        AccountResponse accountResponse = new AccountResponse();
+        BeanUtils.copyProperties(account, accountResponse);
 
-    public AccountResponse updateAccount(long id, AccountRequest accountRequest) throws LineNotUpdatedException, DuplicatedNameException;
+        return accountResponse;
+    }
 
-    public AccountResponse deleteAccount(long id) throws LineNotDeletedException;
+    public List<AccountResponse> getAccounts() {
+
+        List<AccountResponse> ret = new ArrayList<>();
+
+        for (Account account : getXmlMapper().getAccounts()) {
+            AccountResponse accountResponse = new AccountResponse();
+            BeanUtils.copyProperties(account, accountResponse);
+            OptionsResponse optionsResponse = new OptionsResponse();
+            BeanUtils.copyProperties(account.getOptions(), optionsResponse);
+            accountResponse.setOptions(optionsResponse);
+            CurrencyResponse currencyResponse = new CurrencyResponse();
+            BeanUtils.copyProperties(account.getCurrency(), currencyResponse);
+            accountResponse.setCurrency(currencyResponse);
+
+            //
+            // Format balance
+            //
+            String pattern = "#,##0.";
+            for (int i = 0; i < XmlMapper.getHomeBank().getCurrencyMap().get(account.getCurr()).getFrac(); i++) {
+                pattern += "0";
+            }
+            DecimalFormat df = new DecimalFormat(pattern);
+            DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+            symbols.setGroupingSeparator(account.getCurrency().getGchar());
+            symbols.setDecimalSeparator(account.getCurrency().getDchar());
+            df.setDecimalFormatSymbols(symbols);
+            // TODO : Change how symbol is defined and placed
+            // https://stackoverflow.com/questions/29215163/currency-symbol-with-another-number-format
+            accountResponse.setBalance(df.format(account.getBalance()) + " " + account.getCurrency().getSymb());
+
+            ret.add(accountResponse);
+        }
+
+        return ret;
+    }
+
+    public AccountResponse getAccountById(long id) {
+        return null;
+    }
+
+    public AccountResponse updateAccount(long id, AccountRequest accountRequest) throws LineNotUpdatedException, DuplicatedNameException {
+        return null;
+    }
+
+    public AccountResponse deleteAccount(long id) throws LineNotDeletedException {
+        return null;
+    }
 
     //
     // Categories
     //
 
-    public List<CategoryResponse> getCategories();
+    public List<CategoryResponse> getCategories() {
+        List<CategoryResponse> ret = new ArrayList<>();
+
+        for (Category category : getXmlMapper().getCategories()) {
+            CategoryResponse categoryResponse = new CategoryResponse();
+            BeanUtils.copyProperties(category, categoryResponse);
+            ret.add(categoryResponse);
+        }
+
+        return ret;
+    }
 
     //
     // Currencies
+    //
 
-    public List<CurrencyResponse> getCurrencies();
+    public List<CurrencyResponse> getCurrencies() {
+        List<CurrencyResponse> ret = new ArrayList<>();
+
+        for (Currency currency : getXmlMapper().getCurrencies()) {
+            CurrencyResponse currencyResponse = new CurrencyResponse();
+            BeanUtils.copyProperties(currency, currencyResponse);
+            ret.add(currencyResponse);
+        }
+
+        return ret;
+    }
 
     //
     // Favorites
     //
 
-    public List<FavoriteResponse> getFavorites();
+    public List<FavoriteResponse> getFavorites() {
+        List<FavoriteResponse> ret = new ArrayList<>();
+
+        for (Favorite favorite : getXmlMapper().getFavorites()) {
+            FavoriteResponse favoriteResponse = new FavoriteResponse();
+            BeanUtils.copyProperties(favorite, favoriteResponse);
+            ret.add(favoriteResponse);
+        }
+
+        return ret;
+    }
 
     //
     // Operations
     //
 
-    public List<OperationResponse> getOperations();
+    public List<OperationResponse> getOperations() {
+        List<OperationResponse> ret = new ArrayList<>();
+
+        for (Operation operation : getXmlMapper().getOperations()) {
+            OperationResponse operationResponse = new OperationResponse();
+            BeanUtils.copyProperties(operation, operationResponse);
+            ret.add(operationResponse);
+        }
+
+        return ret;
+    }
 
     //
-    // Operations
+    // Payees
     //
 
-    public List<PayeeResponse> getPayees();
+    public List<PayeeResponse> getPayees() {
+        List<PayeeResponse> ret = new ArrayList<>();
+
+        for (Payee payee : getXmlMapper().getPayees()) {
+            PayeeResponse payeeResponse = new PayeeResponse();
+            BeanUtils.copyProperties(payee, payeeResponse);
+            ret.add(payeeResponse);
+        }
+
+        return ret;
+    }
 
     //
     // Tags
     //
 
-    public List<TagResponse> getTags();
+    public List<TagResponse> getTags() {
+        List<TagResponse> ret = new ArrayList<>();
+
+        for (Tag tag : getXmlMapper().getTags()) {
+            TagResponse tagResponse = new TagResponse();
+            BeanUtils.copyProperties(tag, tagResponse);
+            ret.add(tagResponse);
+        }
+
+        return ret;
+    }
 }
