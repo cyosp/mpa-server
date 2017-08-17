50f9ce083d025bd08d71c7b0b770bcb516ef7efa
CYOSP <cyosp@users.noreply.github.com>
2017-08-17 08:27:48 +0200

    Improve UI

--- src/main/java/com/cyosp/mpa/api/rest/common/controller/StaticFilesController.java
+++ src/main/java/com/cyosp/mpa/api/rest/common/controller/StaticFilesController.java
@@ -23,7 +23,11 @@ public class StaticFilesController implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
 
-        String staticDir = "/static";
+        final String DEFAULT_PAGE = "index.html";
+        final String STATIC_DIRECTORY = "/static";
+        final String FORWARD_STRING = "forward:";
+        final String MPA_PREFIX_PATH = "/mpa";
+
         String personalAccountingManaged = null;
 
         if (XmlMapper.getHomeBank() != null) personalAccountingManaged = HomebankRestController.SUB_PATH + "/";
@@ -33,20 +37,26 @@ public class StaticFilesController implements WebMvcConfigurer {
             try {
                 ClassLoader classloader = Thread.currentThread().getContextClassLoader();
                 ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(classloader);
-                Resource[] resources = resolver.getResources("classpath*:" + staticDir + personalAccountingManaged + "**");
+                Resource[] resources = resolver.getResources("classpath*:" + STATIC_DIRECTORY + personalAccountingManaged + "**");
                 for (int i = 0; i < resources.length; i++) {
 
                     String uri = resources[i].getURI().toString();
                     String endPath = uri.substring(uri.indexOf(personalAccountingManaged));
-                    String urlPath = "/mpa/" + endPath.replace(personalAccountingManaged, "");
-                    String viewName = "forward:" + endPath;
+                    String urlPath = MPA_PREFIX_PATH + "/" + endPath.replace(personalAccountingManaged, "");
+                    String viewName = FORWARD_STRING + endPath;
                     if (uri.endsWith("/")) {
                         urlPath = urlPath.substring(0, urlPath.length() - 1);
-                        viewName += "index.html";
+                        viewName += DEFAULT_PAGE;
                     }
+                    // System.out.println(urlPath + " <=> " + viewName);
 
                     registry.addViewController(urlPath).setViewName(viewName);
                 }
+
+                // Add default home page
+                // Example: / => forward:/homebank/v1.2/index.html
+                registry.addViewController(MPA_PREFIX_PATH).setViewName(FORWARD_STRING + personalAccountingManaged + DEFAULT_PAGE);
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
